c209742cf3e15ba95f2eee795c9f0e116d1f1822
GilGuereque <gguereque326@gmail.com>
2024-01-14 19:43:08 -0800

    subscription rates practice problem

---
+++ SubscriptionRates/Program.cs
@@ -0,0 +1,63 @@
+﻿Random random = new Random();
+int daysUntilExpiration = random.Next(12);
+int discountPercentage = 0;
+
+// daysUntilExpiration = 0;
+
+// Your code goes here
+if (daysUntilExpiration <= 10)
+{
+    if (daysUntilExpiration <= 5)
+    {
+        discountPercentage = 10;
+        Console.WriteLine($"Your subscription expires in {daysUntilExpiration} days.");
+        Console.WriteLine($"Renew now and save {discountPercentage}%!");
+    }
+    else if (daysUntilExpiration == 1)
+    {
+        discountPercentage = 20;
+        Console.WriteLine("Your subscription expires within a day!");
+        Console.WriteLine($"Renew now and save {discountPercentage}%!");
+    }
+    else if (daysUntilExpiration == 0)
+    {
+        Console.WriteLine("Your subscription has expired.");
+    }
+    else
+    {
+        Console.WriteLine($"Your subscription will expire soon. In {daysUntilExpiration} days. Renew now!");
+    }
+}
+else 
+{
+    // Console.WriteLine("");
+}
+
+// Correct solution:
+Random random = new Random();
+int daysUntilExpiration = random.Next(12);
+int discountPercentage = 0;
+
+if (daysUntilExpiration == 0)
+{
+    Console.WriteLine("Your subscription has expired.");
+}
+else if (daysUntilExpiration == 1)
+{
+    Console.WriteLine("Your subscription expires within a day!");
+    discountPercentage = 20;
+}
+else if (daysUntilExpiration <= 5)
+{
+    Console.WriteLine($"Your subscription expires in {daysUntilExpiration} days.");
+    discountPercentage = 10;
+}
+else if (daysUntilExpiration <= 10)
+{
+    Console.WriteLine("Your subscription will expire soon. Renew now!");
+}
+
+if (discountPercentage > 0)
+{
+    Console.WriteLine($"Renew now and save {discountPercentage}%.");
+}

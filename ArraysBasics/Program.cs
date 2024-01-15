202042bc42b5588fa6e50642f3bb1995dbede98f
GilGuereque <gguereque326@gmail.com>
2024-01-14 21:10:55 -0800

    Declaring and assigning values to arrays

--- ArraysBasics/Program.cs
+++ ArraysBasics/Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Declare Arrays & Practice
+string[] fraudulentOrderIDs = new string[3];
+// Assign Order ID values to array
+fraudulentOrderIDs[0] = 'A123';
+fraudulentOrderIDs[1] = 'B456';
+fraudulentOrderIDs[2] = 'C789';
+// Attempt to break the array
+fraudulentOrderIDs[3] = "D000";

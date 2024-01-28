1594129bbf95e167c6ad8f4d5f42e0cdeb20087e
GilGuereque <gguereque326@gmail.com>
2024-01-27 17:16:14 -0800

    Updating practice files

--- TestProject/obj/Debug/net8.0/TestProject.AssemblyInfo.cs
+++ TestProject/obj/Debug/net8.0/TestProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aa5c6ee773fe7890397166a210ebae9c498ef2e5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0a288b2e24cc4de0ab8b9ea6e902f30b8e43f9be")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

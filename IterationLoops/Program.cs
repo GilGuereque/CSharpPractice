545ace008b7349e2c0525aa17f293a410a2447d0
GilGuereque <gguereque326@gmail.com>
2024-02-03 19:31:20 -0800

    iteration loop practice

--- IterationLoops/Program.cs
+++ IterationLoops/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// // Practicing with iteration loops:
+// for (int i = 0; i < 10; i++)
+// {
+//     Console.WriteLine(i);
+// }
+
+for (int i = 10; i >= 0; i--)
+{
+    Console.WriteLine(i);
+}

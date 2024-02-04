b0dbde3f545429fd78e807eaa751a7d1264c7e5f
GilGuereque <gguereque326@gmail.com>
2024-02-03 18:17:42 -0800

    Working with variable scope

--- VariableScopes/Program.cs
+++ VariableScopes/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// // exploring global and local variable scopes
+// // create a variable inside of a code block
+// bool flag = true;
+// int value = 10;
+
+// if (flag)
+// {
+//     Console.WriteLine($"Inside the code block: {value}");
+// }
+
+// Console.WriteLine($"Outside the code block: {value}");
+
+// // Using if statements in a code block with or without curly braces:
+// bool flag = true;
+// if (flag)
+// {
+//     Console.WriteLine(flag);
+// }
+
+// Remove the curly braces
+bool flag = true;
+if (flag)
+    Console.WriteLine(flag);

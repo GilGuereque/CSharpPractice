a88b7e2b5c5390acdcf0149b27b280f528cafa52
GilGuereque <gguereque326@gmail.com>
2024-03-28 16:15:15 -0700

    finalized loopinglogic for pets array

--- LoopingLogic/obj/Debug/net8.0/LoopingLogic.AssemblyInfo.cs
+++ LoopingLogic/obj/Debug/net8.0/LoopingLogic.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LoopingLogic")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e39d91b04f382cf8fc5537a69a405795abe179e1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+11de43093a7bbb8ecce6e051b72545689acd4703")]
 [assembly: System.Reflection.AssemblyProductAttribute("LoopingLogic")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LoopingLogic")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

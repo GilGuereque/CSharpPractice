2e83e43f71c2c13210f26ee32ce1c2696199d2f5
GilGuereque <gguereque326@gmail.com>
2024-04-03 20:05:19 -0700

    ToCharArray & Reverse & Split & Join

--- ArrayMethods/Program.cs
+++ ArrayMethods/Program.cs
@@ -16,30 +16,75 @@
 //     Console.WriteLine($"-- {pallet}");
 // }
 
-string[] pallets = { "B14", "A11", "B12", "A13" };
-Console.WriteLine("");
+// string[] pallets = { "B14", "A11", "B12", "A13" };
+// Console.WriteLine("");
 
-foreach (var pallet in pallets)
-{
-    Console.WriteLine($"Before: {pallet}");
-}
+// foreach (var pallet in pallets)
+// {
+//     Console.WriteLine($"Before: {pallet}");
+// }
 
-Array.Clear(pallets, 0, 2);
+// Array.Clear(pallets, 0, 2);
 
-Console.WriteLine($"Clearing 2 ... count: {pallets.Length}");
-foreach (var pallet in pallets)
-{
-    Console.WriteLine($"-- {pallet}");
-}
+// Console.WriteLine($"Clearing 2 ... count: {pallets.Length}");
+// foreach (var pallet in pallets)
+// {
+//     Console.WriteLine($"-- {pallet}");
+// }
 
-Console.WriteLine("");
-Array.Resize(ref pallets, 6);
-Console.WriteLine($"Resizing 6 ... count: {pallets.Length}");
+// Console.WriteLine("");
+// Array.Resize(ref pallets, 6);
+// Console.WriteLine($"Resizing 6 ... count: {pallets.Length}");
 
-pallets[4] = "C01";
-pallets[5] = "C02";
+// pallets[4] = "C01";
+// pallets[5] = "C02";
 
-foreach (var pallet in pallets)
-{
-    Console.WriteLine($"-- {pallet}");
-}
+// foreach (var pallet in pallets)
+// {
+//     Console.WriteLine($"-- {pallet}");
+// }
+
+// string[] pallets = { "B14", "A11", "B12", "A13" };
+// Console.WriteLine("");
+
+// Array.Clear(pallets, 0, 2);
+// Console.WriteLine($"Clearing 2 ... count: {pallets.Length}");
+// foreach (var pallet in pallets)
+// {
+//     Console.WriteLine($"-- {pallet}");
+// }
+
+// Console.WriteLine("");
+// Array.Resize(ref pallets, 6);
+// Console.WriteLine($"Resizing 6 ... count: {pallets.Length}");
+
+// pallets[4] = "C01";
+// pallets[5] = "C02";
+
+// foreach (var pallet in pallets)
+// {
+//     Console.WriteLine($"-- {pallet}");
+// }
+
+// Console.WriteLine("");
+// Array.Resize(ref pallets, 3);
+// Console.WriteLine($"Resizing 3 ... count: {pallets.Length}");
+
+// foreach (var pallet in pallets)
+// {
+//     Console.WriteLine($"-- {pallet}");
+// }
+
+// Using ToCharArray() & Reverse()
+string value = "abc123";
+char[] valueArray = value.ToCharArray();
+Array.Reverse(valueArray);
+string result = new string(valueArray);
+Console.WriteLine(result);
+
+// Using Split() & Join()
+string value2 = "abc123";
+char[] valueArray2 =  value2.ToCharArray();
+Array.Reverse(valueArray2);
+string result2 = string.Join(",", valueArray2);
+Console.WriteLine(result2);
--- ArrayMethods/obj/Debug/net8.0/ArrayMethods.AssemblyInfo.cs
+++ ArrayMethods/obj/Debug/net8.0/ArrayMethods.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ArrayMethods")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+22715219fe03f3d18a16aff67627578a03a59983")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c2d80957f67b706ccfd3249bfd371cfb5f9866ce")]
 [assembly: System.Reflection.AssemblyProductAttribute("ArrayMethods")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ArrayMethods")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

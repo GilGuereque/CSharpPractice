ed74bb650b94b5b493250ef6ae6047dca1c4ef15
GilGuereque <gguereque326@gmail.com>
2024-04-06 19:53:08 -0700

    fortune teller method

--- CreateMethods/obj/Debug/net8.0/CreateMethods.AssemblyInfo.cs
+++ CreateMethods/obj/Debug/net8.0/CreateMethods.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CreateMethods")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0b094d69405638d6abdf38bec185cc1e638d1674")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5dfacf9734bafd708ea2c882687be3ed63d8ddb3")]
 [assembly: System.Reflection.AssemblyProductAttribute("CreateMethods")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CreateMethods")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

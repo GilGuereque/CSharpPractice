eebb0d7ecf4562e9aa1b6584418abac358d27fe4
GilGuereque <gguereque326@gmail.com>
2024-04-07 16:27:29 -0700

    using debugger

--- DebuggingApps/Program.cs
+++ DebuggingApps/Program.cs
@@ -1,6 +1,43 @@
-﻿// added -1 to handle index out of range exception
-string[] students = new string[] {"Sophia", "Nicolas", "Zahirah", "Jeong"};
+﻿// // added -1 to handle index out of range exception
+// string[] students = new string[] {"Sophia", "Nicolas", "Zahirah", "Jeong"};
 
-int studentCount = students.Length;
+// int studentCount = students.Length;
 
-Console.WriteLine("The final name is: " + students[studentCount - 1]); 
+// Console.WriteLine("The final name is: " + students[studentCount - 1]); 
+
+
+// Using debugger
+/* 
+This code uses a names array and corresponding methods to display
+greeting messages
+*/
+
+string[] names = new string[] { "Sophia", "Andrew", "AllGreetings" };
+
+string messageText = "";
+
+foreach (string name in names)
+{
+    if (name == "Sophia")
+        messageText = SophiaMessage();
+    else if (name == "Andrew")
+        messageText = AndrewMessage();
+    else if (name == "AllGreetings")
+        messageText = SophiaMessage();
+        messageText = messageText + "\n\r" + AndrewMessage();
+
+    Console.WriteLine(messageText + "\n\r");
+}
+
+bool pauseCode = true;
+while (pauseCode == true);
+
+static string SophiaMessage()
+{
+    return "Hello, my name is Sophia.";
+}
+
+static string AndrewMessage()
+{
+    return "Hi, my name is Andrew. Good to meet you.";
+}

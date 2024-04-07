729dba9a19e089530a9853ee6a2b318978187854
GilGuereque <gguereque326@gmail.com>
2024-04-07 16:01:09 -0700

    first exception handling

--- DebuggingApps/Program.cs
+++ DebuggingApps/Program.cs
@@ -1,2 +1,6 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// added -1 to handle index out of range exception
+string[] students = new string[] {"Sophia", "Nicolas", "Zahirah", "Jeong"};
+
+int studentCount = students.Length;
+
+Console.WriteLine("The final name is: " + students[studentCount - 1]); 

5c141c84302d62195b686afc38ea911f41c6ad07
GilGuereque <gguereque326@gmail.com>
2024-04-07 12:08:50 -0700

    methods returning booleans

--- CreateMethods/obj/Debug/net8.0/CreateMethods.AssemblyInfo.cs
+++ CreateMethods/obj/Debug/net8.0/CreateMethods.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CreateMethods")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+53513302ac7ff3aacc34c2e0b691c5e344b193b6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+35e20122cead4b6b414bb831bb1913350f060947")]
 [assembly: System.Reflection.AssemblyProductAttribute("CreateMethods")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CreateMethods")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

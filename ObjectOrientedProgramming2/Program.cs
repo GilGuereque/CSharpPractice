88c5c41612c492dc0f3eb333cfb88e19a1f2120a
Gil Guereque <gguereque326@gmail.com>
2025-03-09 19:48:04 -0700

    Practicing with classes, methods, and objects

--- ObjectOrientedProgramming2/Program.cs
+++ ObjectOrientedProgramming2/Program.cs
@@ -1,2 +1,35 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+
+// Instantiate a Person object
+var person1 = new Person("Gil", 33, "Pizza");
+
+// Call the Introduce method
+person1.Introduce();
+
+
+// Pause program
+Console.ReadKey();
+
+// Create Person class with an Introduce method
+class Person
+{
+    public string Name;
+    public int Age;
+    public string FavoriteFood;
+
+    public Person(string name, int age, string favoriteFood)
+    {
+        Name = name;
+        Age = age;
+        FavoriteFood = favoriteFood;
+    }
+
+    public void Introduce() // prints a greeting
+    {
+        Console.WriteLine(
+            $"Hi, my name is {Name}, I am {Age} years old, and I love eating {FavoriteFood}!");
+    }
+}
+

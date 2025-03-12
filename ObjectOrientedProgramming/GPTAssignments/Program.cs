126b3f759f208fd1770ed66ca495a21e2779cc19
Gil Guereque <gguereque326@gmail.com>
2025-03-11 22:10:38 -0700

    Created program to tell a user if their input is even or odd in a while loop

--- ObjectOrientedProgramming/GPTAssignments/Program.cs
+++ ObjectOrientedProgramming/GPTAssignments/Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Hello, World!");
+Console.WriteLine("This program lets you know if a number is even or odd.");
+
+bool userContinue = true;
+
+while (userContinue)
+{
+    Console.WriteLine("\nPlease enter a number, and we will tell you if its even or odd. Enter here: ");
+    string userInput = Console.ReadLine();
+    int number;
+
+    // Parse number and validate its a numerical value
+    if (!int.TryParse(userInput, out number))
+    {
+        Console.WriteLine("You must enter a numerical value. Please try again.");
+            continue; // Skip to the next iteration if parsing fails
+    }
+
+    if (number % 2 == 0)
+    {
+        Console.WriteLine("Even");
+    }
+    else
+    {
+        Console.WriteLine("Odd");
+    }
+    Console.WriteLine("\nPress 0 to exit program, otherwise Enter to continue running it. Enter here: ");
+    string userChoice = Console.ReadLine();
+    if (userChoice == "0")
+    {
+        userContinue = false;
+    }
+}

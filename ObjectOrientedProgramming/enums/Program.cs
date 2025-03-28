930b162a959dac348be56b644a159fe0660d8ada
Gil Guereque <gguereque326@gmail.com>
2025-03-27 21:37:34 -0700

    Casting enums as an int to display the index assigned

--- ObjectOrientedProgramming/enums/Program.cs
+++ ObjectOrientedProgramming/enums/Program.cs
@@ -1,5 +1,12 @@
 ﻿Season firstSeason = Season.Spring;
+Season lastSeason = Season.Winter;
+int firstSeasonAsNumber = (int)firstSeason;
+int lastSeasonAsNumber = (int)lastSeason;
 Console.WriteLine(firstSeason);
+// casting to int works here because enums are actually ints under the hood
+Console.WriteLine("As a number: " + firstSeasonAsNumber); // to display the index of this enum
+Console.WriteLine(lastSeason);
+Console.WriteLine("As a number: " + lastSeasonAsNumber); // to display the index of this enum
 Console.ReadKey();
 
 public enum Season

44af7a9c136f08db60f08c84ce8981c6fad2d196
Gil Guereque <gguereque326@gmail.com>
2025-04-02 20:27:18 -0700

    Virtual methods assignment

---
+++ AdvancedOOP/GetCountsOfAnimals/Program.cs
@@ -0,0 +1,56 @@
+﻿var exercise = new Exercise();
+var legCounts = exercise.GetCountsOfAnimalsLegs();
+
+Console.WriteLine(string.Join(", ", legCounts));
+Console.ReadKey();
+
+public class Exercise
+{
+    public List<int> GetCountsOfAnimalsLegs()
+    {
+        var animals = new List<Animal>
+        {
+            new Lion(),
+            new Tiger(),
+            new Duck(),
+            new Spider()
+        };
+
+        var result = new List<int>();
+        foreach (var animal in animals)
+        {
+            result.Add(animal.NumberOfLegs);
+        }
+        return result;
+    }
+}
+
+public class Animal
+{
+    public virtual int NumberOfLegs { get; } = 4;
+}
+
+public class Lion
+    : Animal
+{
+    public override int NumberOfLegs { get; } = 4;
+}
+
+public class Tiger
+    : Animal
+{
+    public override int NumberOfLegs { get; } = 4;
+}
+
+public class Duck
+    : Animal
+{
+    public override int NumberOfLegs { get; } = 2;
+}
+
+public class Spider
+    : Animal
+{
+    public override int NumberOfLegs { get; } = 8;
+}
+

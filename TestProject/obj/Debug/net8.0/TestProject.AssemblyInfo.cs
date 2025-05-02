c4de501b955b7a14f2cc3316c8119b3eaf823130
Gil Guereque <gguereque326@gmail.com>
2025-05-02 16:32:22 -0700

    Updated build files and pushing last code changes up

--- TestProject/obj/Debug/net8.0/TestProject.AssemblyInfo.cs
+++ TestProject/obj/Debug/net8.0/TestProject.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f208369ad6f22d342488ce5d041d89b90869be78")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1b84ece493e240cc4a3f65e5a27246051aeccfb6")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

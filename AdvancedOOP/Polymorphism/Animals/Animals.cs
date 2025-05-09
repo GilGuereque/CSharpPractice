0f3482e458764e5e5bf04762f72cf3012daf886d
GilGuereque <gguereque326@gmail.com>
2025-05-09 16:09:33 -0700

    Refactored Animals & Flyables classes

--- AdvancedOOP/Polymorphism/Animals/Animals.cs
+++ AdvancedOOP/Polymorphism/Animals/Animals.cs
@@ -1,42 +1,5 @@
 using System;
 
-namespace Polymorphism.Flyables
-{
-    public interface IFlyable
-    {
-        void Fly();
-    }
-    
-    public class Bird : IFlyable
-    {
-        public void Tweet() =>
-            Console.WriteLine("Tweet, tweet!");
-
-        public void Fly() =>
-            Console.WriteLine("Flying using its wings.");
-    }
-
-    public class Kite : IFlyable
-    {
-        public void Fly() =>
-            Console.WriteLine("Flying carried by the wind.");
-    }
-
-    public interface IFeulable
-    {
-        void Fuel();
-    }
-
-    public class Plane : IFlyable, IFeulable
-    {
-        public void Fly() =>
-            Console.WriteLine("Flying using jet propulsion.");
-
-        public void Fuel() =>
-            Console.WriteLine("Filling tanks with jet fuel.");
-    }
-}
-
 namespace Polymorphism.Animals
 {
     interface IAnimal

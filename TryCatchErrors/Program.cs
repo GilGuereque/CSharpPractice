54a7fb9c46fbf9f2461bf49ff524e14c8fe74b7c
GilGuereque <gguereque326@gmail.com>
2025-06-20 15:48:21 -0700

    Practicing Try Catch error handling

---
+++ TryCatchErrors/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+int userCheck = 1;
+
+try
+{
+    if (userCheck == 1)
+    {
+        Console.WriteLine("The first try has succeeded.");
+    }
+}
+catch (Exception e)
+{
+    Console.WriteLine(e);
+}
+
+Console.ReadKey();

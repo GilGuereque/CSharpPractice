e903b4c702ada833dd64e8f368999a95b1ac8d3c
Gil Guereque <gguereque326@gmail.com>
2025-06-21 17:28:49 -0700

    Practicing Try Catch Finally

--- TryCatchErrors/Program.cs
+++ TryCatchErrors/Program.cs
@@ -16,4 +16,27 @@ catch (Exception e)
     Console.WriteLine(e.Message);
 }
 
+
+// Try Catch Finally Practice
+Console.WriteLine("Enter a number:");
+string input = Console.ReadLine();
+try
+{
+    int number = ParseStringToInt(input);
+    Console.WriteLine("String successfully parsed, the result is " + number);
+}
+catch
+{
+    Console.WriteLine("An exception was thrown.");
+}
+finally
+{
+    Console.WriteLine("Finally block is being executed.");
+}
+
 Console.ReadKey();
+
+int ParseStringToInt(string input)
+{
+    return int.Parse(input); // Could also use int.TryParse
+}
